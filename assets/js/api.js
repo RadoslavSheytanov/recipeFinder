dadd9b768567dea072ef119f84ee6746ed3cb7c8
Radoslav Sheytanov <radoslavsheytanov@yahoo.com>
2023-11-03 12:23:03 +0200

    TO-DO JS

---
+++ assets/js/api.js
@@ -0,0 +1,9 @@
+/*
+
+1. Create a function to call the recipe API with the search query.
+2. Handle possible errors from the API call (e.g., no results, API error).
+3. Return the results to main.js.
+
+
+
+*/
---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+/*
+
+1. Listen for search button click or enter keypress.
+2. On click/keypress, get the value from the search input.
+3. Use the value to make an API call (use api.js).
+4. Display the returned results/suggestions in the container.
+
+
+*/
